e074e39e0d8e114863953915403ca31ab57cc6d2
91967 <91967@DESKTOP-09TNRAV>
2022-08-28 18:59:40 +0530

    hi jenkins

---
+++ src/test/java/scripts/Testcase3.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase3 {
+	@Test
+	public void tc3() {
+		Reporter.log("hello appium", true);
+	}
+
+}
